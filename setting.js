5216898085b558f5574bd7df2a5d21e8d5242915
HanzBOTZ <100819969+HANZBOTZGZ@users.noreply.github.com>
2024-11-14 07:25:36 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -2,7 +2,7 @@
 pengaturan lainnya ada di db.setting. anda bisa gunakan command owner
 */
 export default {
-    owner: ["6285157489446"],
+    owner: ["6285215603320"],
     typedb: "json", // use json / mongo
 
     /* database setting */
